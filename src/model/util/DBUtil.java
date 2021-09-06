d08bc060f66678749b37557ca892b0a6c647c64a
jhshin29 <shinji03264@gmail.com>
2021-09-06 18:47:34 +0900

    [Add] structure init

---
+++ src/model/util/DBUtil.java
@@ -0,0 +1,23 @@
+﻿package model.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class DBUtil {
+private static EntityManagerFactory emf;
+	
+	static {
+		emf = Persistence.createEntityManagerFactory("oracleDBUse");
+	}
+	
+	public static EntityManager getEntityManager() {
+		return emf.createEntityManager();
+	}
+	
+	public static void close() {
+		emf.close();
+		emf = null;
+	}
+
+}
